9d7de98a3b3a569a560f3badbcee3ae5e974ae43
Sabariks092 <sabariks092@gmail.com>
2024-10-23 00:56:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import { CurrentWeather } from './Componrnts/CurrentWeather';
+import { Forecast } from './Componrnts/Forecast';
+import '../node_modules/bootstrap/dist/js/bootstrap'
+import { Footer } from './Componrnts/Footer';
+
+function App() {
+  const [city, setCity] = useState();
+  const [clickedCity, setClickedCity] = useState();
+  const [citySuggestion, setCitySuggestion] = useState([]);
+  const [current, setCurrent] = useState();
+  const [forecast, setForecast] = useState();
+  const [location, setLocation] = useState();
+
+  const autoComUrl = 'https://api.weatherapi.com/v1/search.json?key=41d0e5799629443fa07134515241910&q='
+  const weatherURL = (city) => `https://api.weatherapi.com/v1/forecast.json?key=41d0e5799629443fa07134515241910&q=${city}&days=7&aqi=no&alerts=no`
+
+  useEffect(() => {
+    if (city && city.length > 3) {
+      fetchautoComAPI();
+    }
+  }, [city])
+
+  const fetchautoComAPI = async () => {
+    try {
+      const response = await axios.get(autoComUrl + city);
+      const resp = response.data;
+      console.log(resp);
+      const cityData = resp.map((data) => {
+        return `${data.name},${data.region},${data.country}`;
+      });
+      setCitySuggestion(cityData);
+
+
+    }
+    catch (e) {
+      console.log('error', e)
+    }
+  }
+
+
+  const handleSelectedCity = (city) => {
+    console.log('clicked city', city)
+    setClickedCity(city);
+    fetchWeatherURL(city);
+    setCitySuggestion([]);
+
+  }
+
+  const fetchWeatherURL = async (city) => {
+    try {
+      const response = await axios.get(weatherURL(city));
+      const resp = response.data;
+      setCurrent(resp.current);
+      setForecast(resp.forecast);
+      setLocation(resp.location);
+      console.log('Current weather is', resp.current)
+      console.log('forecast weather is', resp.forecast)
+      console.log('location is', resp.location)
+
+    } catch (e) {
+      console.log('Weather error', e)
+
+    }
+  }
+
+  return (
+    <>
+      <div className='container justify-content-center p-5 rounded' style={{ backgroundColor: "rgb(11,19,30)", boxShadow: "9px 9px 12px  rgba(0,0,0,.5)" }} >
+        <div className='d-flex justify-content-center'>
+          <input type="text" value={clickedCity} className='form-control text-center  align-items-center rounded '
+            onChange={(e) => {
+              setCity(e.target.value);
+              if (e.target.value === "") {
+                setCurrent();
+                setForecast();
+                setClickedCity();
+                setLocation();
+              }
+            }} placeholder='Enter your city' style={{ width: "75%" }} />
+        </div>
+        {/* {city&& <h4>{city}</h4>} */}
+        {citySuggestion && citySuggestion.map((city) => {
+          return (
+            <div className='d-flex justify-content-center' >
+              <div className='suggestCity bg-info bg-opacity-10 text-center rounded border-info border-bottom-2 mt-1 text-white fs-2' style={{ cursor: "pointer", width: "75%" }} onClick={() => handleSelectedCity(city)}>
+                <h3 className='sugesstions fs-6 m-2 p-1 '>{city}</h3>
+              </div>
+            </div>
+          )
+        })}
+
+
+        {current && <CurrentWeather current={current} location={location} />}
+        {forecast && <Forecast forecast={forecast} location={location} />}
+
+
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Componrnts/Forecast.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export const Forecast = ({ forecast, location}) => {
+    return (
+        <>
+            <div >
+                <h5 className='mt-4 mb-4 text-center' style={{ color: "white", textAlign: "right" }}>Forecasting weather in <span style={{ color: "rgb(251, 255, 0)" }}>{location.name}</span> for the next <span style={{ color: "rgb(251, 255, 0)" }}>7 days</span></h5>
+            </div>
+            {forecast.forecastday.map((data, index) => {
+                return (
+                    <div className=" justify-content-center accordion mt-1 " id="accordionFlushExample">
+                        <div className="accordion-item">
+                            <h2 className="accordion-header">
+                                <button className="accordion-button collapsed " type="button" data-bs-toggle="collapse" data-bs-target={`#${index}`} aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <div className="d-flex flex-row justify-content-evenly align-items-center text-center" style={{ color: "rgb(11,19,30)", width: "100%" }}>
+                                        <div className="p-2 "><h6>{data.date} </h6></div>
+                                        <div className="p-2"> <img style={{ width: "60px", height: "60px" }} src={data.day.condition.icon} /></div>
+                                        <div className="p-2"><h6>{data.day.condition.text}</h6></div>
+                                        <div className="p-2"><h6> Min temp in c : <span style={{ color: "green" }}> {data.day.mintemp_c}</span> </h6></div>
+                                        <div className="p-2"><h6> Max temp in c : <span  style={{ color: "red" }}>{data.day.maxtemp_c}</span></h6></div>
+                                    </div>
+                                </button>
+                            </h2>
+                            <div id={`${index}`} className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                <div className="accordion-body">
+                                    {data.hour.map((data) => {
+                                        return (
+                                            <>
+                                                <div className="col-12 d-flex justify-content-evenly align-items-center flex-row mb-1" style={{ width: "100%" }}>
+                                                    <div className="p-2 col-2.4"><h6>{data.time}</h6></div>
+                                                    <div className="p-2 col-2.4"><h6>|</h6></div>
+                                                    <div className="p-2 ms-col-2.4" ><img className='' style={{ width: "50px", height: "50px" }} src={data.condition.icon} alt="" /></div>
+                                                    <div className="p-2 col-2.4"><h6>|</h6></div>
+                                                    <div className="p-2 col-2.4"> <h6 className=' text-center '>  
+                                                        {data.condition.text}
+                                                    </h6>
+                                                    </div>
+                                                
+                                                </div>
+
+                                               <div className='mb-4 d-flex justify-content-center'>
+                                                <div className="progress" role="progressbar" aria-label="Example 20px high" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ height: "30px", width:"75%" }}>
+                                                    <div className="progress-bar " style={{  backgroundColor:"rgb(32,43,59)", width: `${data.temp_c}%`, height:"30px" }}>TEMP IN C : {data.temp_c}</div>
+                                                </div>
+                                               </div>
+                                               <hr />
+                                            </>
+
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                )
+            })}
+
+        </>
+    )
+}
